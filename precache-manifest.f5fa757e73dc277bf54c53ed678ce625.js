6b91ac883b7fee430181b1b8501f27a99915fdef
Iyke Prince <iykeprince48@gmail.com>
2020-01-28 15:21:46 +0100

    Updates

---
+++ precache-manifest.f5fa757e73dc277bf54c53ed678ce625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684f21a2b9cba437ea7342e08e77213f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "763f94309f815ef2d5a4",
+    "url": "/monster-rolodex/static/css/main.4eab3f1b.chunk.css"
+  },
+  {
+    "revision": "40f8dec51506c1ca48de",
+    "url": "/monster-rolodex/static/js/2.c55bce54.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.c55bce54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "763f94309f815ef2d5a4",
+    "url": "/monster-rolodex/static/js/main.c2a079f8.chunk.js"
+  },
+  {
+    "revision": "5da687c6ac07fd404d70",
+    "url": "/monster-rolodex/static/js/runtime-main.3440e6cd.js"
+  }
+]);
